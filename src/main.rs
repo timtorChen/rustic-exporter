144b6c2e094825034af8ce8b891ad86ed8eb5129
Timtor Chen <to@timtor.dev>
2024-11-20 14:19:20 +0800

    add log
    - add log-level argument
    - add verbose argument
    - pick `backup.name` config back
    - change metrics label repo_name to use `backup.name`
    - rename var `config` to `config_path`

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use axum::{
 };
 
 use clap::Parser;
+use core::panic;
 use prometheus_client::{encoding::text::encode, registry::Registry};
 use regex::Regex;
 use std::{
@@ -21,6 +22,7 @@ use std::{
     sync::{Arc, Mutex},
 };
 use tokio::signal;
+use tracing::{error, info};
 
 async fn metrics_handler(State(state): State<Arc<Mutex<Registry>>>) -> impl IntoResponse {
     let registry = state.lock().unwrap();
@@ -46,27 +48,69 @@ fn replace_with_env_vars(input: &str) -> String {
 async fn main() {
     let args = cli::Args::parse();
 
-    let mut registry = Registry::default();
+    // log level
+    if args.verbose {
+        tracing_subscriber::fmt().init();
+    } else {
+        let filter =
+            tracing_subscriber::EnvFilter::new(format!("rustic_exporter={}", args.log_level));
+        tracing_subscriber::fmt().with_env_filter(filter).init();
+    }
 
-    let mut file_content = fs::read_to_string(args.config.clone()).unwrap();
+    let config_path = args.config_path;
+    let mut file_content = match fs::read_to_string(config_path.clone()) {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Unable to read the configuration file");
+            panic!("Error: {}", e);
+        }
+    };
+    info!("Using configuration file: {}", config_path);
+
     file_content = replace_with_env_vars(&file_content);
+    let config: Config = match toml::from_str(&file_content) {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Invaid toml file");
+            panic!("Error: {}", e);
+        }
+    };
 
-    let config: Config = toml::from_str(&file_content).unwrap();
+    let mut registry = Registry::default();
     for backup in config.backups {
+        info!("Registering repositroy: {}", backup.name);
         let collector = collector::RusticCollector::new(backup, args.interval);
         registry.register_collector(Box::new(collector));
     }
     let addr = format!("{}:{}", args.host, args.port);
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let listener = match tokio::net::TcpListener::bind(addr.clone()).await {
+        Ok(c) => c,
+        Err(e) => {
+            error!("Cannot listen on {}", addr);
+            panic!("Error: {}", e);
+        }
+    };
     let registry_state = Arc::new(Mutex::new(registry));
     let router = Router::new()
         .route("/metrics", get(metrics_handler))
         .with_state(registry_state);
 
-    axum::serve(listener, router)
-        .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
+    info!("Start server on http://{addr}");
+    let server = axum::serve(listener, router);
+    let server_result;
+    if cfg!(debug_assertions) {
+        server_result = server.await;
+    } else {
+        server_result = server.with_graceful_shutdown(shutdown_signal()).await;
+    }
+
+    match server_result {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Failed to start server");
+            panic!("Error: {}", e);
+        }
+    };
 }
 
 async fn shutdown_signal() {

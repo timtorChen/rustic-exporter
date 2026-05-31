69eeec488ffdd4b9f3a283fac8252ef5e08022f6
Timtor Chen <to@timtor.dev>
2026-05-31 16:27:40 +0800

    drafting

---
+++ tests/functional/test_repository_golden.py
@@ -0,0 +1,15 @@
+# local
+def test_local_repository():
+  pass
+  # start rustic-exporter with local repository config
+  # get the reponse of rustic-exporter
+  # chcek if response matches local data
+
+
+# sftp
+
+# s3
+
+# gcs
+
+# azure
